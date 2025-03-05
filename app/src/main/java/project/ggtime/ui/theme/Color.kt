b63b6e436cbd8ccf1d887794da7594398d9b5edc
Sergio <sergio@dual-link.com>
2025-03-05 10:13:33 +0100

    Initial Commit and project config

---
+++ app/src/main/java/project/ggtime/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package project.ggtime.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Dark Mode Colors
+val MainBackground_Dark = Color(0xFF121212)
+val SecondaryBackground_Dark = Color(0xFF1E1E1E)
+
+val MainText_Dark = Color(0xFFFFFFFF)
+val SecondaryText_Dark = Color(0xFFB0B0B0)
+
+val PrimaryColor_Dark = Color(0xFFFF3B30) // Used to action buttons
+val SecondaryColor_Dark = Color(0xFF3A86FF) // Used to interactive elements
+val AccentColor_Dark = Color(0xFFFFD700) // Used to highlight key elements
+
+// Light Mode Colors
+val Main_Background_Light = Color(0xFFF5F5F5)
+val SecondaryBackground_Light = Color(0xFFFFFFFF)
+
+val MainText_Light = Color(0xFF222222)
+val SecondaryText_Light = Color(0xFF666666)
+
+val PrimaryColor_Light = Color(0xFFFF3B30) // Used to action buttons
+val SecondaryColor_Light = Color(0xFF007AFF) // Used to interactive elements
+val AccentColor_Light = Color(0xFFFFA500) // Used to highlight key elements
---
+++ app/src/main/java/project/ggtime/ui/theme/Strings.kt
@@ -0,0 +1,8 @@
+package project.ggtime.ui.theme
+
+// Spanish
+val Welcome_ES = "Bienvenido";
+
+
+// English
+val Welcome_EN = "Welcome";
